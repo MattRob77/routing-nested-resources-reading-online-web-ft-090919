17c65b5791a53cf5c70c0658140b338f81a64cf3
Matthew Robinson <matt_robinson1@yahoo.com>
2019-11-18 11:43:39 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,13 @@
 class AuthorsController < ApplicationController
 
-  def show
+ def posts_index
     @author = Author.find(params[:id])
+    @posts = @author.posts
+    render template: 'posts/index'
   end
-
+ 
+  def post
+    @author = Author.find(params[:id])
+    
+  
 end
